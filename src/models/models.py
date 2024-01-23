f3fe76292f7f90de7de6a6d2b28e3f956141ebe1
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2024-01-23 10:49:10 -0300

    adicionando Modelos porjeto BLX

---
+++ src/models/models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class Usuario(BaseModel):
+    id: Optional[str] = None
+    nome: str
+    telefone: str
+    meus_produtos: List[Produto]
+    meus_pedidos: List[Pedido]
+    minhas_vendas: List[Pedido]
+    
+    
+class Produto(BaseModel):
+    id: Optional[str] = None
+    usuario: Usuario
+    nome: str
+    detalhe: str
+    preco: float
+    disponivel: bool = False
+    
+    
+class Pedido(BaseModel):
+    id: Optional[str] = None
+    usuaio: Usuario
+    produto: Produto
+    quantidade: int
+    entrega: bool = True
+    endereco: str
+    observacoes = Optional[str] = 'Sem Observacoes'
+    
+    
