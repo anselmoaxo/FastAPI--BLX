2f20d401798ca8557dea5831a02a387b00ff8a0c
Anselmo Xavier <anselmo.cstecnologia@gmail.com>
2024-01-23 22:02:31 -0300

    add requirements.txt

--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -5,14 +5,13 @@ from sqlalchemy.orm import sessionmaker
 
 # Define a URL do banco de dados. Atualmente, está configurado para usar um banco de dados SQLite.
 # Se desejar usar PostgreSQL, a linha comentada abaixo pode ser descomentada.
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app_blx.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./app_blx.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/app_blx"
 
 # Cria uma instância de motor (engine) do SQLAlchemy, que representa a conexão com o banco de dados.
 # O argumento `check_same_thread=False` é utilizado quando se usa SQLite para evitar problemas de concorrência.
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+    SQLALCHEMY_DATABASE_URL)
 
 # Cria uma instância do SessionLocal, que é uma fábrica de sessões do SQLAlchemy.
 # Essa instância será usada para interagir com o banco de dados.
